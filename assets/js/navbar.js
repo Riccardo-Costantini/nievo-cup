91e2027f2d51f83f0a8a15a9aea686476bc8e986
Finoallalba100 <riccardo.costantini.eu@gmail.com>
2026-06-13 02:06:38 +0200

    sistemato header

---
+++ assets/js/navbar.js
@@ -0,0 +1,21 @@
+class NavbarComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="header">
+        <div class="logo">
+          <a href="index.html">
+            <img src="assets/images/loghi/logo_nievo_white.png" alt="Logo Nievo Cup">
+          </a>
+        </div>
+        <div class="menu">
+          <div class="voce"><a href="partite.html"><p>Partite</p></a></div>
+          <div class="voce"><a href="gironi.html"><p>Gironi</p></a></div>
+          <div class="voce" style="display:none;"><p>Tabellone</p></div>
+          <div class="voce"><a href="squadre.html"><p>Squadre</p></a></div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define('custom-navbar', NavbarComponent);
